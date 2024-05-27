282384775e01c3d342dc638e01361ce537835783
Clarence Kinuthia <mclarence7311@gmail.com>
2024-05-27 14:35:34 +0300

    solve anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        sorted_word = sorted(self.word)
+        for w in words:
+            if sorted(w) == sorted_word:
+                anagrams.append(w)
+        return anagrams
+
+        
